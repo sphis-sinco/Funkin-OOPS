bae359b2acf4dd937ea56aff6a4a0e9a4b232c77
Sphis-Sinco <sinconsistencia@gmail.com>
2025-07-05 19:33:52 -0500

    Revert "Muscle memory, jesus."
    This reverts commit ece052a283007a67f9feba4c5cd0b2a7334fe619.

--- source/funkin/ui/MusicBeatState.hx
+++ source/funkin/ui/MusicBeatState.hx
@@ -266,6 +266,6 @@ class MusicBeatState extends FlxTransitionableState implements IEventHandler
 
 enum abstract WatermarkID(String)
 {
-  var LEFT = 'left';
-  var RIGHT = 'right';
+  var LEFT:String = 'left';
+  var RIGHT:String = 'right';
 }
